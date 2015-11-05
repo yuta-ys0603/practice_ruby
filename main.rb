9c7e7150012cbc20a64df02b722b01bed2043ce8
yuta-ys0603 <yuta-ys0603>
2015-11-05 19:39:28 +0900

    Strings Red

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require "./lib/actor/human"
-require "./lib/decorator/character"
+# require "./lib/decorator/character"
+require "./lib/decorator/color_red"
 
 actor = Actor::Human.new 'My name is Hoge Hoge'
-decorator =Decorator::Character.new actor
+# decorator =Decorator::Character.new actor
+decorator = Decorator::ColorRed.new actor
 puts decorator.show_message
 
